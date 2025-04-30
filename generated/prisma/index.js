8cb246cb58f7ba7602d52d79613fe62639437dae
Marc Grupe <marc.grupe@gmail.com>
2025-04-30 14:49:16 -0400

    new migration

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -238,12 +238,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://postgres:root@localhost:5432/capstone?schema=public"
       }
     }
   },
