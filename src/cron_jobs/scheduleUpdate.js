e6f50eb80f37e89be38268cb7b50d7907d2c563f
Marc Grupe <marc.grupe@gmail.com>
2025-05-08 20:07:51 -0400

    add cron jobs to backend

--- src/cron_jobs/scheduleUpdate.js
+++ src/cron_jobs/scheduleUpdate.js
@@ -4,22 +4,34 @@ const { prisma } = require("../common/common");
 
 
 //Schedule a daily task to be run on server .
-cron.schedule('59 23 * * *', function () {
+cron.schedule('00 23 * *', function () {
 
-  const TODAY = new Date();
-  let 
-  const getUsersToUpdate = async (date) => {
+  console.log("STARTED :: Cut Schedule Update");
+  const cutDate = new Date();
+  const newCutDate = new Date(new Date().setDate(new Date().getDate() + 7));
+  
+  console.log('cutDate', cutDate);
+  console.log("newCutDate", newCutDate);
+  
 
-    const users = await prisma.account.findMany({
-      where: {
-        cutDate: { equals: date },
-      },
-    });
-    
+  const updateUsersCutDate = async () => {
+    try {
+      const response = await prisma.account.updateMany({
+        where: {
+          cutDate: { equals: cutDate },
+        },
+        data: {
+          cutDate: newCutDate,
+        },
+      });
+      
+    } catch (error) {
+      console.error(error);      
+    }
+  };
 
-    
-  }
 
+  updateUsersCutDate();
 
-
+  console.log('Completed::Cut Schedule Update');
  });
---
+++ src/cron_jobs/scratch.js
@@ -0,0 +1,5 @@
+let date = new Date();
+date = date.toISOString().split('T')[0];
+
+
+console.log('date',date);
