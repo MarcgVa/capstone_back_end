133c830b51bbd9a9adbf2ce446d55639b5c608ad
Marc Grupe <marc.grupe@gmail.com>
2025-05-23 09:49:26 -0400

    deploying to prod updating cors settings

--- server/server.js
+++ server/server.js
@@ -2,12 +2,13 @@ const app = require("./app");
 require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const cors = require("cors");
+app.use(cors());
 
-if (process.env.ENV === "dev") {
-  app.use(cors({ origin: /localhost/ }));
-} else {
-  app.use(cors({ origin: "https://grupelawncare.netlify.app" }));
-}
+// if (process.env.ENV === "dev") {
+//   app.use(cors({ origin: /localhost/ }));
+// } else {
+//   app.use(cors({ origin: "https://grupelawncare.netlify.app" }));
+// }
 
 
 app.listen(PORT, () => {
